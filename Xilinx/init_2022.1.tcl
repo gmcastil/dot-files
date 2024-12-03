7f25a0acb7a38353afc269f1e849d503878edf47
George Castillo <4260836+gmcastil@users.noreply.github.com>
2024-12-03 06:07:26 -0700

    Recent modifications

--- Xilinx/init_2022.1.tcl
+++ Xilinx/init_2022.1.tcl
@@ -1,4 +1,4 @@
 set_param board.repoPaths \
   [list \
-    "/home/castillo/github-repos/Digilent/vivado-boards" \
+    "/home/castillo/github-repos/Digilent/vivado-boards/new/board_files" \
     "/home/castillo/github-repos/Avnet/bdf"]
